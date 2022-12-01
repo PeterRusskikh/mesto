6d10d8fe196f98f33918922e319f0ea7b7651da2
Peter Russkikh <Trad696969@gmail.com>
2022-12-01 19:53:27 +0300

    feat: сделать попап

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let popupOpened = document.querySelector('.popup_opened');
+let popup = document.querySelector('.popup');
+let buttonAdd = document.querySelector('.button_add');
+let buttonClose = document.querySelector('.button_close');
+
+function openPop() {
+
+}
+
+buttonAdd.addEventListener('click', openPop);
